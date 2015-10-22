f4780c0db38ec486a52880f446f85b3fd479d9b8
coffeeexistence <askinganthony@gmail.com>
2015-10-22 19:11:25 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,8 @@
 # Build your say_hello method here
+def say_hello(greetingName=(no_argument_passed=true; nil))
+  if no_argument_passed
+    puts "Hello Ruby Programmer!"
+  else
+    puts "Hello #{greetingName}!"
+  end
+end
