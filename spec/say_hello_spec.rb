7484dcd5f5442b98f3fed5cea38efafe23be92ed
gaccrux <venividivici47bc@hotmail.com>
2015-11-09 14:33:53 +0000

    Done.

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -4,7 +4,6 @@ describe "say_hello" do
 
   it 'accepts an argument of a name and prints out Hello with that Name' do
     expect($stdout).to receive(:puts).with("Hello #{Name}!")
-    say_hello("name")
   end
 
   it 'defaults to Ruby Programmer when no name is passed in' do
@@ -12,6 +11,4 @@ describe "say_hello" do
     say_hello()
   end
 end
-def say_hello(name)
-  puts "Hello #{name}!"
-end
+
