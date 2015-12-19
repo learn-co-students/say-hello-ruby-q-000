cf8833a6afefd2ab3d228c7589e1a5c41f88de56
crf2907 <cfrancis275@googlemail.com>
2015-12-19 18:01:32 +0000

    nitrous-backup at Sat Dec 19 18:01:32 UTC 2015

--- say_hello.rb
+++ say_hello.rb
@@ -3,3 +3,29 @@ def say_hello(name="Ruby Programmer")
 end
 say_hello
 say_hello("Gabriela")
+
+
+
+
+
+The board follows the following format:
+When the board is displayed there are three characters per cell. An empty cell is "   " and a filled cell is " X "
+Three cells per a row.
+The middle cell in a row is bordered by 2 | (pipes): O | X |
+Three rows on the board.
+Rows are separated by: -----------
+
+board = [" "," "," "," "," "," "," "," "," "]
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+display_board(board)
+
+An empty board would be:
+board = [" "," "," "," "," "," "," "," "," "]
+If we called display_board and passed in that board as an argument
+
+board = [" "," "," "," "," "," "," "," "," "]
+display_board(board)
