5ec2a9a98417d1e75820d496a4b12daeec7b57c8
Ayesha Bajwa <abajwa@mit.edu>
2016-03-26 10:06:39 -0700

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,9 @@
 # Build your say_hello method here
+def say_hello(name = "Ruby Programmer")
+	puts "Hello " + name + "!"
+	#puts "Hello #{name}!"
+	#note to self: have to use puts in order to get the new line!
+end
+
+#print say_hello("Ayesha")
+#print say_hello("Kent Beck")
